f14a6eeddc07305b8809a85b80b07864edd4ca02
Ash <ads04r@ecs.soton.ac.uk>
2018-09-21 17:07:37 +0100

    Added CRC functionality

--- htdocs/functions.php
+++ htdocs/functions.php
@@ -11,7 +11,7 @@ function spoof_data($postdata)
 		$chunk = substr($postdata, 0, 32);
 		$postdata = substr($postdata, 32);
 
-		$random_weight = pack("V", rand(50000, 150000));
+		$random_weight = pack("V", rand(114000, 150000));
 		$user_id = unpack("V", substr($chunk, 14, 4))[1];
 
 		if($user_id == 0)
@@ -23,6 +23,10 @@ function spoof_data($postdata)
 	}
 	$ret .= $postdata;
 
+	$ret = substr($ret, 0, (strlen($ret) - 2))
+	$crc = crc16($ret);
+	$ret .= pack("V", $crc)
+
 	return($ret);
 }
 
@@ -32,3 +36,22 @@ function get_timestamp()
 	$ret = pack("V", $dt);
 	return($ret);
 }
+
+function crc16($buffer)
+{
+	$result = 0x0000;
+	if (($length = strlen($buffer)) > 0)
+	{
+		for ($offset = 0; $offset < $length; $offset++)
+		{
+			$result ^= (ord($buffer[$offset]) << 8);
+			for ($bitwise = 0; $bitwise < 8; $bitwise++)
+			{
+				if (($result <<= 1) & 0x10000) { $result ^= 0x1021; }
+				$result &= 0xFFFF;
+			}
+		}
+	}
+	return $result;
+}
+
